f44546e1394e5d76b4dc537781e4d9514795e809
DenuccioDiego <diegodenuccio95@gmail.com>
2022-05-02 11:53:22 +0200

    ADD crud

--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Product } from 'src/app/shared/model/product-model';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, ObservableInput } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable()
@@ -16,5 +16,24 @@ export class ProductService {
     return this.http.get<Product[]>(url);
   }
 
+  public getOneProduct(id: number): Observable<Product> {
+    const url = environment.baseUrl + '/products/' + id;
+    return this.http.get<Product>(url);
+  }
+
+  public deleteProduct(id: number): Observable<any> {
+    const url = environment.baseUrl + '/products/' + id;
+    return this.http.delete<any>(url);
+  }
+
+  public editProduct(product: Product): Observable<Product> {
+    const url = environment.baseUrl + '/products/' + product.id;
+    return this.http.put<Product>(url, product);
+  }
+
+  public createProduct(product: Product): Observable<any> {
+    const url = environment.baseUrl + '/products'
+    return this.http.post(url, product)
+  }
 
 }
--- src/app/features/products/components/product-container/product-container.component.ts
+++ src/app/features/products/components/product-container/product-container.component.ts
@@ -15,28 +15,49 @@ export class ProductContainerComponent implements OnInit {
   }
 
   constructor(private productService: ProductService) {
+
     this.productService.getAllProducts().subscribe(
       result => {
         this.productList = result;
       }, error => {
         console.log(error);
       }
-    )
+    );
+
   }
 
-  deleteFunction(object: Object, args: any[]) {
-    const indexOfObject = args.findIndex((e) => {
-      return e === object;
-    });
-    args.splice(indexOfObject, 1);
-  }
+
+
+  /*  deleteFunction(object: Object, args: any[]) {
+     const indexOfObject = args.findIndex((e) => {
+       return e === object;
+     });
+     args.splice(indexOfObject, 1);
+   } */
 
   detailProductSelectionHandler(product: Product): void {
-    console.table(product);
+    this.productService.getOneProduct(product.id).subscribe(
+      result => {
+        var product = result;
+        console.log(product)
+      }, error => {
+        console.log(error);
+      }
+    );
+
+    console.log(product.id);
   }
 
   deleteProductSelectionHandler(product: Product): void {
-    this.deleteFunction(product, this.productList);
+
+    this.productService.deleteProduct(product.id).subscribe(
+      response => {
+        this.productList = this.productList.filter(el => el.id !== product.id)
+      }, error => {
+        console.log(error)
+      }
+    )
+    //this.deleteFunction(product, this.productList);
     console.table(product);
   }
 
